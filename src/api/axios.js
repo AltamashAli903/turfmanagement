38f3d27864c906bcd025a6102a79e063ce74009b
AltamashAli903 <sayyadaltamash26@gmail.com>
2026-07-15 10:34:26 +0530

    Url

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:4500/api"
-  baseURL: "http://192.168.1.17:4500/api"
-    //  baseURL: "https://turf-backend-mtku.onrender.com/api"
+  // baseURL: "http://192.168.1.17:4500/api"
+     baseURL: "https://turf-backend-mtku.onrender.com/api"
 });
 
 API.interceptors.request.use((config) => {
